451a898024bac748e05107858c3bb3d09ca1f9aa
Deepika7067 <126557852+Deepika7067@users.noreply.github.com>
2023-04-15 23:44:39 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -2,7 +2,23 @@
 //input: Anitha
 //output: Aahint 
 
-int main()
 {
-  return 0;
+	char string[100];
+  printf("\n\t Enter the string : ");
+	scanf(“%s”,string);
+	char temp;
+	int i, j;
+	int n = strlen(string);
+	for (i = 0; i < n-1; i++) {
+		for (j = i+1; j < n; j++) {
+			if (string[i] > string[j]) {
+					temp = string[i];
+					string[i] = string[j];
+					string[j] = temp;
+			}
+		}
+	}
+
+	printf(“The sorted string is : %s”, string);
+	return 0;
 }
