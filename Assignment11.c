6715a04dc6a5ef7a5d09957ef45a0966d076488a
Deepika7067 <126557852+Deepika7067@users.noreply.github.com>
2023-04-16 00:08:36 +0530

    Update Assignment11.c

--- Assignment11.c
+++ Assignment11.c
@@ -1,8 +1,18 @@
 //Convert an decimal to its binary equivalent using a bitwise operator
 
+int binary(int n)
+{
+    for (int i = 6; i >= 0; i--) {
+        int k = n >> i; 
+        if (k & 1) 
+              printf("1");
+        else printf("0");
+    }
+}
+ 
 int main()
 {
-  int N = 7;
-  
+    int n = 7;
+    binary(n);
   return 0;
 }
